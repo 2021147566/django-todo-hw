f4120c7f47a1db7db7c1c968b3d83444d9ad6968
2021147566 <eaudechance@yonsei.ac.kr>
2023-09-07 12:48:41 +0900

    add post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,14 @@
+# tweet/models.py
 from django.db import models
+from user.models import UserModel
+
 
 # Create your models here.
+class PostModel(models.Model):
+    class Meta:
+        db_table = "post"
+
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    title = models.TextField(max_length=256)
+    content = models.TextField(max_length=256)
